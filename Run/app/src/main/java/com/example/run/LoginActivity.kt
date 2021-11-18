92f4e811096d822ce19c9bb70f4bd5fecc4969ac
Alberto Serrato <albertoserrato@hotmail.com>
2021-11-17 23:10:07 -0500

    actividad registro con validaciones completas,  al pasar validaciones se realiza registro a firebase,  si el mail ya est aregistrado indica error

--- Run/app/src/main/java/com/example/run/LoginActivity.kt
+++ Run/app/src/main/java/com/example/run/LoginActivity.kt
@@ -3,19 +3,33 @@ package com.example.run
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    // autenticación con firebase
+    private lateinit var auth: FirebaseAuth;
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
-
-
         // Splash
-        Thread.sleep(2000) //Hack:
+        Thread.sleep(1000) //Hack:
         setTheme(R.style.Theme_Design_NoActionBar)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance();
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLoginLogin)
+        val loginEmail = findViewById<EditText>(R.id.editTextLoginEmail)
+        val loginPassword = findViewById<EditText>(R.id.editTextLoginPassword)
 
         // funcion que lleva a registro con el boton inscribirse
         val loginSignUp = findViewById<TextView>(R.id.textViewLoginSignUp)
@@ -23,7 +37,26 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+        buttonLogin.setOnClickListener {
+            signInuser(loginEmail.text.trim().toString(),loginPassword.text.trim().toString())
 
+        }
+
+
+    }
+
+    private fun signInuser(email:String, password: String) {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this){  task ->
+
+            if (task.isSuccessful){
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this, "An error occurred", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
 }
